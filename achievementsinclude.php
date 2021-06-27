4c17b43e5044b8adbf24d7ade9f7359899028aed
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-06-27 03:03:37 -0700

    pg_query()

--- achievementsinclude.php
+++ achievementsinclude.php
@@ -17,7 +17,7 @@ if($conn === false){
   echo "<br>";
 }
 $sql1 = "SELECT * FROM persons";  
-$result = $conn->pg_query($sql1);
+$result = pg_query($conn, $sql1);
 echo '<h2 style="text-align: center; color: white;">Achievements</h1>';
 echo '<style>
 .score {
