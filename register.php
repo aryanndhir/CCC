558136e2a575786011c90cf1843bfa48f648e355
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-06-27 01:45:15 -0700

    posgre

--- register.php
+++ register.php
@@ -10,12 +10,8 @@ if(isset($_POST["submit"]))
 
 function OpenCon()
  {
-    $dbhost = "sql6.freesqldatabase.com";
-    $dbuser = "sql6400897";
-    $dbpass = "gcysFbCvd9";
-    $db = "sql6400897";
-    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
-    return $conn;
+	$conn = pg_connect(string $_ENV['DATABASE_URL']);
+ 	return $conn;
  }
  
 function CloseCon($conn)
